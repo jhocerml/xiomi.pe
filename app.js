ed7d7bab9956bef7302dd103ce2c9f5e8aefda2e
jhocerml <jmoncin2002@gmail.com>
2026-03-13 00:05:38 -0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,9 @@
+/* Código para el botón tipo hamburguesa en dispositivos móviles */
+document.addEventListener('DOMContentLoaded', () => {
+    const burger = document.querySelector('.burger');
+    const navLinks = document.querySelector('.nav-links');
+
+    burger.addEventListener('click', () => {
+        navLinks.classList.toggle('active');
+    });
+});
